447a7311dcfb0660c757ee4cbf05d10067b70ead
BlackSheep <gaetanos31@hotmail.fr>
2023-05-17 20:14:34 +0200

    change root to app

--- Kasa/src/app.jsx
+++ Kasa/src/app.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import router from './router.jsx'
+import "./utils/scss/app.scss"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('app')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
