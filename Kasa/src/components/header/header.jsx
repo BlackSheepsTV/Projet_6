a4ef8938d3a41e8d69a7a7ff769db617b598b66c
BlackSheep <gaetanos31@hotmail.fr>
2023-05-17 20:03:53 +0200

    Router + Starting header

---
+++ Kasa/src/components/header/header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+      
+        <header>
+          <nav>
+            <ul>
+                <li>Acceuil</li>
+                <li>A propos</li>
+            </ul>
+          </nav>
+        </header>
+        
+    );
+  }
+  
+  export default Header;
---
+++ Kasa/src/pages/notFound.jsx
@@ -0,0 +1,11 @@
+function NotFound() {
+    return (
+      
+        <div>
+          <h1>Sorry this page doesnt exist</h1>
+        </div>
+        
+    );
+  }
+  
+  export default NotFound;
