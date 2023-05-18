5d46f490b5c37a30c135abd0545ac66e3dcb8888
BlackSheep <gaetanos31@hotmail.fr>
2023-05-19 00:43:05 +0200

    rename dropdownmenu and new components

---
+++ src/components/dropdownMenu.jsx
@@ -0,0 +1,19 @@
+import "../utils/scss/about.scss"
+import PropTypes from 'prop-types';
+
+export default function DropdownMenu({ title, text }) {
+  return (
+      <div className="dropdown-wrapper">
+        <div className="dropdown-title">
+            <p>{title}</p>
+           <div className="arrow"></div>
+        </div>
+        <p>{text}</p>
+      </div>
+  );
+}
+
+DropdownMenu.propTypes = {
+    title: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+};
--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,19 +1,17 @@
 import "../../utils/scss/about.scss"
 import HomeHeader from "../../components/page-header/page-header";
-import AboutMenu from "./about-menu";
+import DropdownMenu from "../../components/dropdownMenu.jsx";
 import  { menus } from "../../data/about.js"
 
 export default function Home() {
   return (
       <>
         <HomeHeader />
-        <div className="az">
+        <div className="about-menus">
             {menus.map((menu, index) => (
-                <AboutMenu key={index} title={menu.title} text={menu.text} />
+                <DropdownMenu key={index} title={menu.title} text={menu.text} />
             ))}
         </div>
-        
-        
       </>
   );
 }
