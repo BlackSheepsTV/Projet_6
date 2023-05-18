ed9bace4a7c743615b6f9afc84df239436e2a01e
BlackSheep <gaetanos31@hotmail.fr>
2023-05-18 23:25:25 +0200

    page header new components

---
+++ src/components/page-header/page-header.jsx
@@ -0,0 +1,9 @@
+import "../../utils/scss/home.scss"
+
+export default function HomeHeader() {
+  return (
+        <div className="home-header">
+          <p>Chez vous, partout et ailleurs</p>
+        </div>
+  );
+}
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,13 +1,13 @@
 import "../../utils/scss/home.scss"
 import Card from "./card";
+import HomeHeader from "../../components/page-header/page-header";
 import { Logements }  from "../../data/logement.js"
 
 export default function Home() {
   return (
       <>
-        <div className="home-header">
-          <p>Chez vous, partout et ailleurs</p>
-        </div>
+        <HomeHeader />
+        
         <div className='cards-wrapper'>
           {Logements.map((logement, index ) => (
             <Card key={index} img={logement.cover} location={logement.location} />
