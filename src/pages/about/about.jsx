7a91203c408ac9539037770f626fb839c0f89cc9
BlackSheep <gaetanos31@hotmail.fr>
2023-05-19 00:13:36 +0200

    about responsive finish

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,11 +1,17 @@
 import "../../utils/scss/about.scss"
 import HomeHeader from "../../components/page-header/page-header";
+import AboutMenu from "./about-menu";
+import  { menus } from "../../data/about.js"
 
 export default function Home() {
   return (
       <>
         <HomeHeader />
-        <p>About</p>
+        <div className="az">
+            {menus.map((menu, index) => (
+                <AboutMenu key={index} title={menu.title} text={menu.text} />
+            ))}
+        </div>
         
         
       </>
--- src/router.jsx
+++ src/router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./components/Layout/layout.jsx"
 import Home from "./pages/home/home.jsx";
+import About from "./pages/about/about.jsx"
 import NotFound from "./pages/notFound";
 
 const router = createBrowserRouter([
@@ -11,6 +12,11 @@ const router = createBrowserRouter([
               path: "/",
               element: <Home />,
             },
+
+            {
+                path: "/about",
+                element: <About />,
+              },
 
             {
                 path: "*",
