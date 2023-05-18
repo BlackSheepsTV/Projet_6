72153687b9763eddedad03350da8aa579352009b
BlackSheep <gaetanos31@hotmail.fr>
2023-05-18 18:36:18 +0200

    notfound finish

--- src/pages/notFound.jsx
+++ src/pages/notFound.jsx
@@ -1,11 +1,13 @@
-function NotFound() {
+import { Link } from 'react-router-dom';
+import '../utils/scss/notFound.scss'
+
+export default function NotFound () {
     return (
-      
-        <div>
-          <h1>Sorry this page doesnt exist</h1>
+        <div className='not-found'>
+          <h1>404</h1>
+          <p>Oups! La page que vous demandez n&apos;existe pas.</p>
+
+          <Link to="/">Retourner sur la page d&apos;accueil</Link>
         </div>
-        
     );
-  }
-  
-  export default NotFound;
+}
