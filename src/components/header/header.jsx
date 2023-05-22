02edc4a9573d89d91860f10ea4a94569b775ce5c
BlackSheep <gaetanos31@hotmail.fr>
2023-05-22 19:43:21 +0200

    Adding logo to header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,11 @@
 import { NavLink } from 'react-router-dom';
-import '../../utils/scss/header.scss'
+import '../../utils/scss/header.scss';
+import logo from '../../assets/logo.svg';
 
 export default function Header() {
     return (
         <header>
-            <h1 className='kasa'>Kasa</h1>
+            <img src={logo} alt="logo kasa" />
             <nav>
                 <ul>
                     <li><NavLink to={"/"} className={({ isActive }) =>  isActive ? "nav-active" : ""}>Acceuil</NavLink></li>
