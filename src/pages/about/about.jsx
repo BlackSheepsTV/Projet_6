57bac6c7dea81d43ba04c571febe5d05e4c2204d
BlackSheep <gaetanos31@hotmail.fr>
2023-05-22 20:10:29 +0200

    adding img

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -2,11 +2,12 @@ import "../../utils/scss/dropdown.scss"
 import HomeHeader from "../../components/page-header/page-header";
 import DropdownMenu from "../../components/dropdownMenu.jsx";
 import  { menus } from "../../data/about.js"
+import aboutImg from '../../assets/kalen-emsley-Bkci_8qcdvQ-unsplash 2.jpg'
 
 export default function Home() {
   return (
       <>
-        <HomeHeader />
+        <HomeHeader img={aboutImg} />
         <div className="about-menus">
             {menus.map((menu, index) => (
                 <DropdownMenu key={index} title={menu.title} text={menu.text} />
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,11 +2,12 @@ import "../../utils/scss/home.scss"
 import Card from "../../components/card/card.jsx";
 import HomeHeader from "../../components/page-header/page-header";
 import { Logements }  from "../../data/logement.js"
+import homeImg from '../../assets/home.png';
 
 export default function Home() {
   return (
       <>
-        <HomeHeader text="Chez vous, partout et ailleurs"/>
+        <HomeHeader text="Chez vous, partout et ailleurs" img={homeImg} />
 
         <div className='cards-wrapper'>
           {Logements.map((logement, index ) => (
