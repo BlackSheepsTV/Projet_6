3058eb2122b61b0f8d074676e6e1ca8cb4ea72ac
BlackSheep <gaetanos31@hotmail.fr>
2023-05-22 20:09:57 +0200

    change classname to card

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 export default function Card({ img, location}) {
     return(
-        <div className="card-wrapper">
+        <div className="card">
             <img src={img} alt="Photo de logement" />
             <p>{location}</p>
         </div> 
