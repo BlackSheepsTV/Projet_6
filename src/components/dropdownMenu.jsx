27be0117564cc3cc94bab904d06c5849f3e6fac8
BlackSheep <gaetanos31@hotmail.fr>
2023-05-22 20:54:48 +0200

    Dropdown working

--- src/components/dropdownMenu.jsx
+++ src/components/dropdownMenu.jsx
@@ -1,14 +1,25 @@
 import "../utils/scss/dropdown.scss"
+import { useState } from "react";
 import PropTypes from 'prop-types';
+import arrowDown from '../assets/arrowDown.svg'
+import arrowUp from '../assets/arrowUp.svg'
 
 export default function DropdownMenu({ title, text }) {
+
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
       <div className="dropdown-wrapper">
-        <div className="dropdown-title">
-            <p>{title}</p>
-           <div className="arrow"></div>
+        <div className="dropdown-title" onClick={() => setIsOpen(!isOpen)}>
+          <p>{title}</p>
+          {!isOpen ? (
+            <img src={arrowDown} alt="arrowDown" />
+          ) : (
+            <img src={arrowUp} alt="arrowUp" />
+
+          )}
         </div>
-        <p>{text}</p>
+        {isOpen && <p>{text}</p> }
       </div>
   );
 }
