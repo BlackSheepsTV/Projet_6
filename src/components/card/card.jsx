ec4a97e8d9832965c6527e5f8689166493d4df60
BlackSheep <gaetanos31@hotmail.fr>
2023-05-24 20:14:12 +0200

    transformTitle to lowercase no space

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,9 +1,13 @@
 import '../../utils/scss/card.scss'
 import PropTypes from 'prop-types';
+import { useNavigate } from "react-router-dom";
 
 export default function Card({ img, title}) {
+    const navigate = useNavigate()
+    const transformedTitle = title.replace(/[\s-]+/g, '-').toLowerCase();
+
     return(
-        <div className="card">
+        <div className="card" onClick={() => navigate(`/logement/${transformedTitle}`)}>
             <img src={img} alt={title} />
             <p>{title}</p>
         </div> 
